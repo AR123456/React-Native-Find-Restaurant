55d8f6678d37083447354dd05217ccd638132169
AR123456 <coloradoz99@gmail.com>
2020-06-30 07:04:27 -0600

    searchBar icon

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-
+// using the icon library that is included out of box with the
+// the expo library
+// https://icons.expo.fyi/
+import { Feather } from "@expo/vector-icons";
 const SearchBar = () => {
   return (
     <View style={styles.background}>
+      <Feather name="search" size={30} color="black" />
       <Text>Search Bar</Text>
     </View>
   );
 };
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: "grey", // #F0EEEE
+    backgroundColor: "#F0EEEE", //
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15,
