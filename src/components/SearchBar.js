19e260b432e44b2c4727de13c3738f7404e5adf8
AR123456 <coloradoz99@gmail.com>
2020-06-30 07:04:59 -0600

    style search bar flex

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,41 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+// for a text input box use TextInput, not Text
+import { View, TextInput, StyleSheet } from "react-native";
 // using the icon library that is included out of box with the
 // the expo library
 // https://icons.expo.fyi/
 import { Feather } from "@expo/vector-icons";
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
+    <View style={styles.backgroundStyle}>
       <Feather name="search" size={30} color="black" />
-      <Text>Search Bar</Text>
+      {/* no default styling is applied to TextInput  */}
+      <TextInput
+        // placeholder prop
+        placeholder="Search"
+        style={styles.inputStyle}
+      />
     </View>
   );
 };
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
     backgroundColor: "#F0EEEE", //
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15,
+    // to get child elements to display on the same line
+    // horizontal direction controlled by the justify
+    //content property which at this point is flexStart
+    // need to change this in child to flex: 1
+    flexDirection: "row",
+  },
+  inputStyle: {
+    borderColor: "black",
+    borderWidth: 1,
+    // text input is being smashed down in the horizontal direction
+    // due to flex start so set flex 1
+    flex: 1,
   },
 });
 export default SearchBar;
