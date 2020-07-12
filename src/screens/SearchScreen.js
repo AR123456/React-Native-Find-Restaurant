2a0682df4c47fe4e0ca7cb245cc4c5ae97f15d47
AR123456 <coloradoz99@gmail.com>
2020-07-12 07:44:34 -0600

    constraining view elements

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -17,9 +17,14 @@ const SearchScreen = () => {
   // React Native ScrollView controls the vertical scroll-
   // if it detects that it has too much content to fit on the
   // screen all at one time it will enable scrolling. So here wrap ResutlList with ScrollView
-
+  // android devices may need additional configuration . Constrain view element to only use screen state
+  // that is avalible  with flex box, flex property can be applied to a child to tell that
+  // element to fill up and expand to visible space that is avalible
+  // https://www.udemy.com/course/the-complete-react-native-and-redux-course/learn/lecture/15707270#overview
+  // flex:1 does this  add it to the most parent view.  A flex:1 to parent element
+  //can fix a lot of vertical scrolling issues
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <SearchBar
         term={term}
         onTermChange={setTerm}
