a4b53e5bd0cdf450d8c798f1602a64458b8b9051
AR123456 <coloradoz99@gmail.com>
2020-07-15 06:46:24 -0600

    Fetch a single restaurant

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 // props come directly from a parent component get navigation which has
 // a special function getParam
 const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
   const id = navigation.getParam("id");
+  console.log(result);
+  // helper function
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+  useEffect(() => {
+    getResult(id);
+    // only want to do this one time
+  }, []);
   console.log(id);
   return (
     <View>
@@ -13,3 +25,4 @@ const ResultsShowScreen = ({ navigation }) => {
 };
 const styles = StyleSheet.create({});
 export default ResultsShowScreen;
+// whenever talking about rendering components it is state that we are talking about
